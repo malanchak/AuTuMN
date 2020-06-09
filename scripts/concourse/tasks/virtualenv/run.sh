13643dfdb74d595a385c1c7cc1824c2c931e1060
Matthew Segal <mattdsegal@gmail.com>
2020-06-09 17:47:12 +1000

    Zzz

--- scripts/concourse/tasks/virtualenv/run.sh
+++ scripts/concourse/tasks/virtualenv/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+echo "Installing virtualenv"
+pip install virtualenv
+
+echo "Creating virtualenv"
 cp -r autumn-repo autumn-env
 cd autumn-env
-pip install virtualenv
+pwd
+ls -la
 virtualenv env
+ls -la
 . env/bin/activate
 pip install -r ./scripts/aws/infra/requirements.txt
 ./scripts/aws/run.sh
