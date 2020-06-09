39b32685edb2a7ba89a0d25431d2427b586fe4b3
Matthew Segal <mattdsegal@gmail.com>
2020-06-09 17:41:29 +1000

    add virutalenv task

---
+++ scripts/concourse/tasks/virtualenv/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mv autumn-repo autumn-env
+cd autumn-env
+pip install virtualenv
+virtualenv env
+. env/bin/activate
+pip install -r ./scripts/aws/infra/requirements.txt
+./scripts/aws/run.sh
