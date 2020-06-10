518ea34731ea33a2974e233a7f1eb4938cddd55c
Matthew Segal <mattdsegal@gmail.com>
2020-06-10 15:51:02 +1000

    zz

--- scripts/concourse/tasks/aws-status/run.sh
+++ scripts/concourse/tasks/aws-status/run.sh
@@ -6,15 +6,16 @@ which python
 . venv/bin/activate
 which python
 deactivate
+venv/bin/python -V
 
-cp -r venv autumn-repo/scripts/aws/env
-echo "autumn repo"
-ls autumn-repo/scripts/aws
-ls autumn-repo/scripts/aws/env
-which python
-. autumn-repo/scripts/aws/env/bin/activate
-which python
-deactivate
+# cp -r venv autumn-repo/scripts/aws/env
+# echo "autumn repo"
+# ls autumn-repo/scripts/aws
+# ls autumn-repo/scripts/aws/env
+# which python
+# . autumn-repo/scripts/aws/env/bin/activate
+# which python
+# deactivate
 
 #echo "this is new"
 #autumn-repo/scripts/aws/run.sh status
