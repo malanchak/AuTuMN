173d91beb19a31fff5e82062f526bd3a41c4e17d
Matthew Segal <mattdsegal@gmail.com>
2020-06-10 16:43:33 +1000

    Add scripts

---
+++ scripts/concourse/tasks/calibrate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Calibration script to be run in Concourse
+set -e
+MODEL=$1
+NUM_CHAINS=15
+RUN_TIME=30 # Seconds
+JOB_NAME=$MODEL-$RANDOM
+autumn-repo/scripts/aws/run.sh run calibrate \
+    $JOB_NAME \
+    $MODEL \
+    $NUM_CHAINS \
+    $RUN_TIME | tee calibration.log
+
+# Check output log for run name.
+MATCH="Calibration finished for"
+RUN_NAME=$(sed -n "/$MATCH/p" calibration.log | cut -d' ' -f4)
+if [ -z "$RUN_NAME" ]
+then
+      echo "Completed run $RUN_NAME"
+      echo "$RUN_NAME" > run-name.log
+else
+      echo "Run for $MODEL failed."
+      exit 1
+fi
