e5ffd161dbc62d4fe02e64dc9969b78ce116529a
Matthew Segal <mattdsegal@gmail.com>
2020-06-10 15:12:02 +1000

    try caching

--- scripts/concourse/tasks/virtualenv/run.sh
+++ scripts/concourse/tasks/virtualenv/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-echo "Installing virtualenv"
-pip install virtualenv
-
-echo "Creating virtualenv"
-virtualenv venv
+echo "Installing AWS CLI script requirements."
+if [ ! -d "venv" ]
+then
+    echo "No virtualenv found - creating a new one."
+    pip install virtualenv
+    virtualenv venv
+else
+    echo "Already found virtualenv."
+fi
+echo "Installing requirements"
 . venv/bin/activate
 pip install -r autumn-repo/scripts/aws/infra/requirements.txt
 deactivate
