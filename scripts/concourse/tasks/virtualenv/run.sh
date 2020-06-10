1420be41961ee1e7acbf24bd1a8ce9825255a426
Matthew Segal <mattdsegal@gmail.com>
2020-06-10 12:48:43 +1000

    give it a crack

--- scripts/concourse/tasks/virtualenv/run.sh
+++ scripts/concourse/tasks/virtualenv/run.sh
@@ -6,10 +6,9 @@ echo "Creating virtualenv"
 virtualenv venv
 . venv/bin/activate
 pip install -r autumn-repo/scripts/aws/infra/requirements.txt
+deactivate
 
-echo "Testing virtualenv"
-which python
-echo "Testing virtualenv freeze"
-pip freeze
+echo "Testing virtualenv copy"
+cp -r venv autumn-repo/scripts/aws/env
 echo "Testing virtualenv on script"
 autumn-repo/scripts/aws/run.sh
