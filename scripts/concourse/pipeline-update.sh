72314ba8af13b49ba1e47eb0510e70149db25fe3
Matthew Segal <mattdsegal@gmail.com>
2020-06-10 14:40:36 +1000

    add AWS access key

--- scripts/concourse/pipeline-update.sh
+++ scripts/concourse/pipeline-update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # Update all the concourse pipelines
 TARGET=autumn
-fly -t $TARGET sp -c pipelines/pipeline.yml -p test
+. ./envars.secret.sh
+
+fly -t $TARGET sp \
+    -c pipelines/pipeline.yml \
+    -v aws_secret_access_key=$AWS_SECRET_ACCESS_KEY \
+    -v aws_access_key_id=$AWS_ACCESS_KEY_ID \
+    -p test
