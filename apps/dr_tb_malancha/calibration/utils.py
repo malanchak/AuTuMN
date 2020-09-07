33af838304118e16d4046bcd114b5e5036adda72
Malancha Karmakar <mkarmakar@b004912-dla.bhri.internal>
2020-09-07 16:43:24 +1000

    added rate of reinfection once recovered parameter, updated range for INH

--- apps/dr_tb_malancha/calibration/utils.py
+++ apps/dr_tb_malancha/calibration/utils.py
@@ -1,7 +1,7 @@
 
 def get_prior_distributions():
     par_priors = [
-        {"param_name": "beta", "distribution": "uniform", "distri_params": [3.0, 7.0],},
+        {"param_name": "beta", "distribution": "uniform", "distri_params": [3.0, 12.0],},
         {"param_name": "epsilon", "distribution": "lognormal", "distri_params": [-6.78, 0.15]},
         {"param_name": "kappa", "distribution": "lognormal", "distri_params": [-4.50, 0.13]},
         {"param_name": "nu", "distribution": "lognormal", "distri_params": [-11.99, 0.34]},
@@ -13,9 +13,14 @@ def get_prior_distributions():
             "distri_ci": [.06, 1.06],
         },
         {
+        	"param_name": "rr_reinfection_once_recovered",
+        	"distribution": "uniform",
+        	"distri_ci": [0.5, 1.5],
+    	},
+        {
             "param_name": "prop_of_failures_developing_inh_R",
             "distribution": "uniform",
-            "distri_params": [.01, .99],
+            "distri_params": [.01, 0.99],
         },
         {
             "param_name": "prop_of_failures_developing_rif_R",
